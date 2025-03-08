074126c6fece2e4edf4bafae0d437244fd78dcf1
Myles Strickler <mylesstrickler@gmail.com>
2025-03-08 14:36:51 -0500

    Initial Commit - Project Template

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chip8_emulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
